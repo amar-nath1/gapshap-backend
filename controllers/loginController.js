0056529ad2ea3b51a4c3b8aeabc579290e3e3a4e
amar-nath1 <amarat279955@gmail.com>
2024-08-25 16:54:40 +0530

    sending jwt after login

---
+++ controllers/loginController.js
@@ -0,0 +1,36 @@
+// controllers/userController.js
+
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+require('dotenv').config();
+const loginUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    // Check if the user already exists
+    const existingUser = await User.findByEmail(email);
+    if (existingUser) {
+      const secretKey = crypto.randomBytes(64).toString('hex');
+
+      const { password } = req.body;
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch) {
+        return res.status(401).json({ message: 'User not authorized' });
+    }
+    else {
+      const token = jwt.sign({ emailId: email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+    }
+    
+    }
+    else {
+      return res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: 'Internal bhak server error' });
+  }
+};
+
+module.exports = { loginUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,13 @@
 
 const express = require('express');
 const { registerUser } = require('../controllers/userController');
+const { loginUser } = require('../controllers/loginController');
 
 const router = express.Router();
 
 // Route for user signup
+router.post('/login', loginUser);
 router.post('/signup', registerUser);
+
 
 module.exports = router;
